f550cad9602005290b3ff6dee05020279bb8ee0a
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-05-18 11:15:06 -0400

    Added user-login Registration login

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -9,7 +9,7 @@ const apiUrl = 'https://rmilligansmovieapp.herokuapp.com/';
 @Injectable({
   providedIn: 'root'
 })
-export class UserRegistrationService {
+export class FetchApiDataService {
   // Inject the HttpClient module to the constructor params
  // This will provide HttpClient to the entire class, making it available via this.http
   constructor(private http: HttpClient) {
@@ -140,7 +140,7 @@ export class UserRegistrationService {
       }
 
       // Non-typed response extraction
-  private extractResponseData(res: Response): any {
+      private extractResponseData(res: any): any {
     const body = res;
     return body || { };
   }
